eabfd1459d9ed06bf372af7fcdc3527c55694dda
Matthew Stratford <hi@mstratford.net>
2019-02-02 20:11:55 +0000

    Remove more sports in JS.

--- public/js/cg.js
+++ public/js/cg.js
@@ -126,38 +126,6 @@ app.controller('lowerThirdsCtrl', ['$scope', 'socket',
     }
 ]);
 
-app.controller('archeryCtrl', ['$scope', 'socket',
-    function($scope, socket){
-        socket.on("archery", function (msg) {
-            $scope.archery = msg;
-        });
-    }
-]);
-
-app.controller('boxingCtrl', ['$scope', 'socket',
-    function($scope, socket){
-
-        socket.on("boxing", function (msg) {
-            $scope.boxing = msg;
-        });
-
-        socket.on("clock:tick", function (msg) {
-            $scope.clock = msg.slice(0, msg.indexOf("."));
-        });
-
-        $scope.$watch('boxing', function() {
-            if (!$scope.boxing) {
-                getBoxingData();
-            }
-        }, true);
-
-        function getBoxingData() {
-            socket.emit("boxing:get");
-            socket.emit("clock:get");
-        }
-    }
-]);
-
 app.controller('bugCtrl', ['$scope', '$timeout', 'socket',
     function($scope, $timeout, socket){
         $scope.tickInterval = 1000; //ms
@@ -187,109 +155,6 @@ app.controller('bugCtrl', ['$scope', '$timeout', 'socket',
 
         // Start the timer
         $timeout(tick, $scope.tickInterval);
-    }
-]);
-
-app.controller('scoringCtrl', ['$scope', '$interval', '$http', 'socket',
-    function($scope, $interval, $http, socket){
-        $scope.tickInterval = 5000;
-        $scope.yorkScore = "";
-        $scope.lancScore = "";
-
-        var fetchScore = function () {
-          var config = {headers:  {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-            }
-          };
-
-          $http.get('https://roseslive.co.uk/score.json', config)
-            .success(function(data) {
-              if(isNaN(data.york) || isNaN(data.lancs)){
-                console.log("Roses live is giving us nonsense");
-                return;
-              };
-              if(!$scope.manualScore){
-                $scope.yorkScore = data.york;
-                $scope.lancScore = data.lancs;
-              };
-                socket.emit('lancScore', data.lancs);
-                socket.emit('yorkScore', data.york);
-            }
-          );
-        };
-
-        socket.on("score", function (state) {
-            $scope.showScore = state.showScore;
-            $scope.manualScore = state.manualScore;
-            if(state.manualScore){
-              $scope.yorkScore = state.yorkScore;
-              $scope.lancScore = state.lancScore;
-            };
-        });
-
-        $scope.$watch('score', function() {
-            if (!$scope.score) {
-                getScoreData();
-            }
-        }, true);
-
-        function getScoreData() {
-            socket.emit("score:get");
-        }
-
-        //Intial fetch
-        fetchScore();
-        // Start the timer
-        $interval(fetchScore, $scope.tickInterval);
-    }
-]);
-
-app.controller('footballCtrl', ['$scope', 'socket',
-    function($scope, socket){
-
-        socket.on("football", function (msg) {
-            $scope.football = msg;
-        });
-
-        socket.on("clock:tick", function (msg) {
-            $scope.clock = msg.slice(0, msg.indexOf("."));
-        });
-
-        $scope.$watch('football', function() {
-            if (!$scope.football) {
-                getFootballData();
-            }
-        }, true);
-
-        function getFootballData() {
-            socket.emit("football:get");
-            socket.emit("clock:get");
-        }
-    }
-]);
-
-app.controller('rugbyCtrl', ['$scope', 'socket',
-    function($scope, socket){
-
-        socket.on("rugby", function (msg) {
-            $scope.rugby = msg;
-        });
-
-        socket.on("clock:tick", function (msg) {
-            $scope.clock = msg.slice(0, msg.indexOf("."));
-        });
-
-        $scope.$watch('rugby', function() {
-            if (!$scope.rugby) {
-                getRugbyData();
-            }
-        }, true);
-
-        function getRugbyData() {
-            socket.emit("rugby:get");
-            socket.emit("clock:get");
-        }
     }
 ]);
 
@@ -381,24 +246,6 @@ app.controller('socialmediaCtrl', ['$scope', '$http', 'socket', '$sce',
 
 ]);
 
-app.controller('dartsCtrl', ['$scope', 'socket',
-    function($scope, socket){
-        socket.on("dart", function (msg) {
-            $scope.darts = msg;
-        });
-
-        $scope.$watch('dart', function() {
-            if (!$scope.dart) {
-                getDartData();
-            }
-        }, true);
-
-        function getDartData() {
-            socket.emit("dart:get");
-        }
-    }
-]);
-
 app.controller('gridCtrl', ['$scope', 'socket',
     function($scope, socket){
         socket.on("grid", function (payload) {
@@ -411,76 +258,5 @@ app.controller('gridCtrl', ['$scope', 'socket',
                 $scope.grid = payload;
             }
         });
-    }
-]);
-
-app.controller('swimmingCtrl', ['$scope', 'socket',
-    function($scope, socket){
-        socket.on("swimming", function (msg) {
-            $scope.swimming = msg;
-        });
-
-        $scope.clockMin = "0";
-        $scope.clockSec = "00";
-        $scope.clockDec = "0";
-
-        socket.on("clock:tick", function (msg) {
-            $scope.clockMin = msg.slice(0,msg.indexOf(":")).replace(/^0/, '');
-            $scope.clockSec = msg.slice(msg.indexOf(":")+1,msg.indexOf("."));
-            $scope.clockDec = msg.slice(msg.indexOf(".")+1);
-        });
-
-        $scope.$watch('swimming', function() {
-            if (!$scope.swimming) {
-                getSwimmingData();
-            }
-        }, true);
-
-        function getSwimmingData() {
-            socket.emit("swimming:get");
-            socket.emit("clock:get");
-        }
-    }
-]);
-
-app.controller('basketballCtrl', ['$scope', 'socket',
-    function($scope, socket){
-
-        socket.on("basketball", function (msg) {
-            $scope.basketball = msg;
-        });
-
-        socket.on("clock:tick", function (msg) {
-            $scope.clock = msg.slice(0, msg.indexOf("."));
-        });
-
-        $scope.$watch('basketball', function() {
-            if (!$scope.basketball) {
-                getBasketballData();
-            }
-        }, true);
-
-        function getBasketballData() {
-            socket.emit("basketball:get");
-            socket.emit("clock:get");
-        }
-    }
-]);
-
-app.controller('badmintonCtrl', ['$scope', 'socket',
-    function($scope, socket){
-        socket.on("badminton", function (msg) {
-            $scope.badminton = msg;
-        });
-
-        $scope.$watch('badminton', function() {
-            if (!$scope.badminton) {
-                getBadmintonData();
-            }
-        }, true);
-
-        function getBadmintonData() {
-            socket.emit("badminton:get");
-        }
     }
 ]);
